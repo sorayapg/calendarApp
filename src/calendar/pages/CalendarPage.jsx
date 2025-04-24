b48cdea532d6bad5d37acf36bd75e793aeb48611
Soraya Povedano <72301609+sorayapg@users.noreply.github.com>
2025-04-24 18:42:44 +0200

    Refactor CalendarPage to integrate event handling and improve calendar functionality

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,11 +1,49 @@
+import { Calendar, dateFnsLocalizer } from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+import { format, addHours, parse, startOfWeek, getDay } from 'date-fns';
+import enUS from 'date-fns/locale/en-US';
+
+
 import { Navbar } from '../';
 
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+const locales = {
+  'en-US': enUS,
+};
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+});
+
+const events = [{
+  title: 'Cumpleaños de Diego',
+  notes: 'Organizar el cumple',
+  start: new Date(),
+  end: addHours(new Date(), 2),
+  bgColor: '#fafafa',
+  user: {
+    _id: '123',
+    name: 'Diego'
+  }
+}];
 
 export const CalendarPage = () => {
   return (
     <>
       <Navbar />
-    
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 'calc( 100vh - 80px )' }}
+      />
     </>
-  )
+  );
 }
