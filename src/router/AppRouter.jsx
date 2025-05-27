606b8bd38a85ff6279aa6916dcd8db3a46507829
Soraya Povedano <72301609+sorayapg@users.noreply.github.com>
2025-05-27 11:10:42 +0200

    Update authStatus in AppRouter to reflect not-authenticated state

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -6,7 +6,7 @@ import { CalendarPage } from '../calendar';
 
 export const AppRouter = () => {
 
-    const authStatus = 'authenticated' //'not-authenticated'  // 'not-authenticated' | 'authenticated' | 'checking'
+    const authStatus = 'not-authenticated' //'not-authenticated'  // 'not-authenticated' | 'authenticated' | 'checking'
 
     return (
         <Routes>
