3ff2b1c076f51d2978cb2ddbbef802d8e45b3a29
Soraya Povedano <72301609+sorayapg@users.noreply.github.com>
2025-05-27 10:20:46 +0200

    Refactor store imports and reorder reducers for clarity

--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { uiSlice,  } from './ui/uiSlice'; // ✅ evita la circularidad
-import { calendarSlice } from './calendar/calendarSlice';
-
-
+import { uiSlice, calendarSlice, authSlice  } from './'; // ✅ evita la circularidad
+// Importa los slices desde el índice de la carpeta store
 export const store = configureStore({
     reducer: {
-        calendar: calendarSlice.reducer,
-        ui: uiSlice.reducer,
+        auth:       authSlice.reducer,
+        calendar:   calendarSlice.reducer,
+        ui:         uiSlice.reducer,
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware({
         serializableCheck: false
