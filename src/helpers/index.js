59541191365c2518142dd6421f27c2f4f34d535a
Soraya Povedano <72301609+sorayapg@users.noreply.github.com>
2025-05-27 09:48:56 +0200

    Add getEnvVariables helper function to retrieve environment variables

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,4 +1,5 @@
 // archivo de barril para expotar todo lo que se necesite de la carpeta helpers
 
 export * from './calendarLocalizer';
+export * from './getEnvVariables';
 export * from './getMessages';
