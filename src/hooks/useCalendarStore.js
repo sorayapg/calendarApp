114f7c3031ae8591a16af584b94e15bb96e55c45
Soraya Povedano <72301609+sorayapg@users.noreply.github.com>
2025-05-28 18:09:15 +0200

    Refactor useCalendarStore to add event loading functionality and improve event handling

--- src/hooks/useCalendarStore.js
+++ src/hooks/useCalendarStore.js
@@ -1,6 +1,7 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { onAddNewEvent, onDeleteEvent, onSetActiveEvent, onUpdateEvent } from '../store';
 import { calendarApi } from '../api';
+import { convertEventsToDateEvents } from '../helpers';
 
 
 export const useCalendarStore = () => {
@@ -35,6 +36,20 @@ export const useCalendarStore = () => {
         dispatch ( onDeleteEvent() );
     }
 
+    const starrtLoadingEvents = async () => {
+        try {
+
+            const {data } = await calendarApi.get('/events');
+            const events = convertEventsToDateEvents( data.events );
+            console.log( events );
+            
+        } catch (error) {
+            console.log('Error al cargar eventos');
+            console.log(error);
+            
+        }
+    }
+
 
     return {
         //* Propiedades
@@ -44,8 +59,9 @@ export const useCalendarStore = () => {
 
         //* Métodos
         setActiveEvent,
-        startSavingEvent,
         startDeletingEvent,
+        starrtLoadingEvents,
+        startSavingEvent,
     }
   
 }
