5aa8d7a650c14ff20f5a91d57c39b6d8236df15c
Soraya Povedano <72301609+sorayapg@users.noreply.github.com>
2025-05-28 12:42:31 +0200

    Refactor Navbar component to display user name and enhance logout functionality

--- src/calendar/components/Navbar.jsx
+++ src/calendar/components/Navbar.jsx
@@ -1,16 +1,32 @@
+import { useAuthStore } from '../../hooks'
 
 
 export const Navbar = () => {
+  
+  const { startLogout, user } =  useAuthStore(); // Extraemos el estado de autenticación y las funciones necesarias del store de autenticación
+  
+  
+  
+  
+  
+  // Este componente regresa la barra de navegacion de la aplicación, que incluye el nombre de la aplicación y un boton de salir
+
+
+
   return (
     <div className="navbar navbar-dark bg-dark mb-4 px-4">
         <span className="navbar-brand">
             <i className="fas fa-calendar-alt"></i>
             &nbsp;
-            Soraya
+            { user.name }
         </span>
 
-        <button className="btn btn-outline-danger">
+        <button 
+          className="btn btn-outline-danger"
+          onClick={ startLogout } // Al hacer click en el botón, se ejecuta la función del logout
+        >
             <i className="fas fa-sign-out-alt"></i>
+            &nbsp;
             <span>Salir</span>
 
         </button>
