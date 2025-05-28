56e502b68011864c64e597d78efe7ce3b98e3c9c
Soraya Povedano <72301609+sorayapg@users.noreply.github.com>
2025-05-28 18:09:31 +0200

    Refactor index.js to include convertEventsToDateEvents in exports

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,5 +1,6 @@
 // archivo de barril para expotar todo lo que se necesite de la carpeta helpers
 
 export * from './calendarLocalizer';
+export * from './convertEventsToDateEvents';
 export * from './getEnvVariables';
 export * from './getMessages';
