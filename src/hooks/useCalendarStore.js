c90d1ddd27a244d006e4a4d604c875574d02096d
Soraya Povedano <72301609+sorayapg@users.noreply.github.com>
2025-06-23 16:42:36 +0200

    Refactor event handling in useCalendarStore and calendarSlice to improve event updates and deletions

--- src/hooks/useCalendarStore.js
+++ src/hooks/useCalendarStore.js
@@ -2,6 +2,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { onAddNewEvent, onDeleteEvent, onLoadEvents, onSetActiveEvent, onUpdateEvent } from '../store';
 import { calendarApi } from '../api';
 import { convertEventsToDateEvents } from '../helpers';
+import Swal from 'sweetalert2';
 
 
 export const useCalendarStore = () => {
@@ -16,19 +17,29 @@ export const useCalendarStore = () => {
     }
 
     const startSavingEvent = async( calendarEvent ) => {
-        // llegar al backend 
-
-        // Todo: Update event 
-
-        if ( calendarEvent._id) {
+       
+        try {
+            if ( calendarEvent.id) {
             // Actualizando un evento
-            dispatch( onUpdateEvent({ ...calendarEvent }));
-        } else {
-            // Creando un nuevo evento
-            const { data } = await calendarApi.post('/events', calendarEvent)
-            //console.log(data);
-            dispatch ( onAddNewEvent({ ...calendarEvent, id: data.event.id, user }));
+
+            await calendarApi.put(`/events/${calendarEvent.id }`, calendarEvent)
+            dispatch( onUpdateEvent({ ...calendarEvent, user }));
+            return;
+        } 
+
+        // Creando un nuevo evento
+         const { data } = await calendarApi.post('/events', calendarEvent)
+         //console.log(data);
+         dispatch ( onAddNewEvent({ ...calendarEvent, id: data.event.id, user }));
+
+        } catch (error) {
+            console.log(error);
+            Swal.fire('Error al guardar', error.response.data.msg, 'error');
         }
+
+
+        
+        
     }
 
     const startDeletingEvent = () => {
