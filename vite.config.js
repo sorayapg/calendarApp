f982eee265ef770fb11e5eaef2a4962e17eda257
Soraya Povedano <72301609+sorayapg@users.noreply.github.com>
2026-04-19 19:01:07 +0200

    feat: update Vite configuration to set base path based on environment mode

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/calendarApp/',
+export default defineConfig(({ mode }) => ({
+  base: mode === 'development' ? '/' : '/calendarApp/',
   plugins: [react()],
-})
+}))
